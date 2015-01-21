025dc08351e99c3bc9d05fcec448d0eff4dd1a99
kruegz <krueger.zach@gmail.com>
2015-01-20 19:51:06 -0600

    Big update, should be fully functional

--- lights.py
+++ lights.py
@@ -1,18 +1,47 @@
 import web
 import serial
 
-#ser = serial.Serial('/dev/ttyACM0', 9600)
+ser = serial.Serial('/dev/ttyACM0', 9600)
 
 render = web.template.render('templates/')
 
 urls = (
-    '/', 'index'
+	'/', 'index'
 )
 
+floorLamp = False
+deskLamp = False
+bedLamp = False
+
 class index:
-    def GET(self):
-        return render.index()
+	def GET(self):
+		global floorLamp
+		global deskLamp
+		global bedLamp
+
+		data = web.input(c=None)
+
+		if data.c == "floorLamp" and floorLamp:
+			ser.write("floorLampOff")
+			floorLamp = False;
+		elif data.c == "floorLamp" and not floorLamp:
+			ser.write("floorLampOn")
+			floorLamp = True;
+		elif data.c == "deskLamp" and deskLamp:
+			ser.write("deskLampOff")
+			deskLamp = False;
+		elif data.c == "deskLamp" and not deskLamp:
+			ser.write("deskLampOn")
+			deskLamp = True;
+		elif data.c == "bedLamp" and bedLamp:
+			ser.write("bedLampOn")
+			bedLamp = False;
+		elif data.c == "bedLamp" and not bedLamp:
+			ser.write("bedLampOn")
+			bedLamp = True;
+
+		return render.index(data.c, floorLamp, deskLamp, bedLamp)
 
 if __name__ == "__main__":
-    app = web.application(urls, globals())
-    app.run()
+	app = web.application(urls, globals())
+	app.run()
